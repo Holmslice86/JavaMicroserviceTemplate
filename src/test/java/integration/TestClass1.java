9119425961280f9fd4694c4f10e9d51b74a489d7
Andrew Holm <Andrew.Holm@carvana.com>
2017-07-13 10:29:13 -0700

    testing a failing test commit

--- src/test/java/integration/TestClass1.java
+++ src/test/java/integration/TestClass1.java
@@ -1,3 +1,5 @@
+package integration;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -5,6 +7,6 @@ import org.junit.Test;
 public class TestClass1 {
     @Test
     public void testClass_runs_test() {
-        Assert.assertTrue(true);
+        Assert.assertTrue(false);
     }
 }
