c0f30a8e0ef5b4788d1ae7b8843162d32ddff932
Andrew Holm <Andrew.Holm@carvana.com>
2017-07-12 17:13:42 -0700

    added initial test

---
+++ src/test/java/integration/GreetingControllerTests.java
@@ -0,0 +1,22 @@
+package integration;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GreetingControllerTests {
+
+    public GreetingControllerTests() {
+
+
+    }
+
+    @Test
+    public void greetingController_returns_greeting() {
+        Assert.assertTrue(true);
+    }
+}
+
+
+
+
